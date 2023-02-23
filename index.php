4434e1c21e7fdef4b81810d079526b32cbafce06
Marco Hernandez <marco7245@hotmail.com>
2023-02-22 20:14:57 -0600

    Iniciando Proyecto

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Encuentra a tu Mascota Ideal</title>
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="./css/bootstrap.min.css">
+    <link rel="stylesheet" href="./css/app.css">
+
+</head>
+
+<body>
+    <header class="container-fluid text-center p-5 bg-primary text-light">
+        <h1>Adopción Responsable Petco</h1>
+    </header>
+
+    <main>
+        <section class="container-fluid p-5">
+            <div class="row">
+                <div class="col-lg-6 text-center">
+                    <a class="btn btn-outline-primary" href="Mascotas.php">Sección de Mascotas</a>
+                </div>
+
+                <div class="col-lg-6 text-center">
+                    <a class="btn btn-outline-primary" href="Usuarios.php">Sección de Usuarios</a>
+                </div>
+            </div>
+        </section>
+
+        <section class="container-fluid p-5">
+            <h2 class="text-center display-5 text-primary text-uppercase"> Mascotas Saludables, Personas Felices</h2>
+            <h3 class="text-center display-3 text-danger text-uppercase">Un mundo Mejor</h3>
+
+            <p class="p-5 fs-5 text-center">
+                En Petco los animales son primero, por ello somos amantes de las mascotas y conocemos el fuerte vínculo emocional que existe entre una mascota y su familia.<br><br>
+
+                Todos los días, puedes adoptar una mascota en una tienda. Petco y nuestros socios comunitarios, diariamente ayudan a encontrar hogares para miles de perros, gatos, conejos y otras mascotas todas las semanas.
+            </p>
+        </section>
+    </main>
+
+</body>
+
+</html>
